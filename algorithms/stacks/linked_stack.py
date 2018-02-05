6029a8e40d3f691b474a0f5a284c8a598e33f708
Zhijun Sheng <zhijunsheng@gmail.com>
2018-02-05 14:54:22 -0500

    implemented ArrayStack and LinkedStack

---
+++ algorithms/stacks/linked_stack.py
@@ -0,0 +1,86 @@
+import unittest
+
+class Empty(Exception):
+    pass
+
+class LinkedStack:
+    """LIFO Stack implementation using a singly linked list for storage."""
+
+    #--------------------- nested _Node class ----------------------
+    class _Node:
+        """Lightweight, nonpublic class for storing a singly linked node."""
+        __slots__ = '_element', '_next'     # streamline memory usage
+
+        def __init__(self, element, next):  # initialize node's fields
+            self._element = element         # reference to user's element
+            self._next = next               # reference to next node
+
+    #--------------------- stack methods ----------------------
+    def __init__(self):
+        """Create an empty stack."""
+        self._head = None                   # reference to the head node
+        self._size = 0                      # number of stack elements
+
+    def __len__(self):
+        """Return the number of elements in the stack."""
+        return self._size
+
+    def is_empty(self):
+        """Return True if the stack is empty."""
+        return self._size == 0
+
+    def push(self, e):
+        """Add element e to the top of the stack."""
+        self._head = self._Node(e, self._head)  # create and link a new node
+        self._size += 1
+
+    def top(self):
+        """Return (but do not remove) the element at the top of the stack.
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._head._element          # top of stack is at head of list
+
+    def pop(self):
+        """Remove and return the element from the top of the stack (i.e., LIFO).
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        answer = self._head
+        self._head = self._head._next       # bypass the former top node
+        self._size -= 1
+        return answer
+
+class TestLinkedStack(unittest.TestCase):
+
+    def test_linked_stack_empty(self):
+        s = LinkedStack()
+        self.assertTrue(s.is_empty())
+
+    def test_linked_stack_push_top(self):
+        s = LinkedStack()
+        self.assertTrue(s.is_empty())
+        s.push(7)
+        self.assertFalse(s.is_empty())
+        self.assertEqual(7, s.top())
+
+    def test_linked_stack_push_pop(self):
+        s = LinkedStack()
+        self.assertTrue(s.is_empty())
+        s.push(7)
+        self.assertEqual(1, len(s))
+        self.assertEqual(7, s.pop()._element)
+        self.assertEqual(0, len(s))
+
+    def test_linked_stack_raise_exception(self):
+        s = LinkedStack()
+        self.assertTrue(s.is_empty())
+        self.assertRaises(Empty, s.top)
+        self.assertRaises(Empty, s.pop)
+
+if __name__ == '__main__':
+    unittest.main()
