7fc6fecd516f627460386ed3c2e51dfead66fdde
Zhijun Sheng <zhijunsheng@gmail.com>
2018-02-07 21:15:57 -0500

    implemented PositionalList

--- algorithms/trees/tree.py
+++ algorithms/trees/tree.py
@@ -77,6 +77,41 @@ class Tree:
             p = self.root()
         return self._height2(p)    # start _height2 recursion
 
+    def __iter__(self):
+        """Generate an iteration of the tree's elements."""
+        for p in self.positions():      # use same order as positons()
+            yield p.element()           # but yield each element
+
+    def preorder(self):
+        """Generate a preorder iteration of positions in the tree."""
+        if not self.is_empty():
+            for p in self._subtree_preorder(self.root()):   # start recursion
+                yield p
+
+    def _subtree_preorder(self, p):
+        """Generate a preorder iteration of positions in subtree rooted at p."""
+        yield p                                     # visit p before its subtrees
+        for c in self.children(p):                  # for each child c
+            for other in self._subtree_preorder(c): # do preorder of c's subtree
+                yield other                         # yielding each to our caller
+
+    def posisions(self):
+        """Generate an iteration of the tree's positions."""
+        return self.preorder()                      # return entire preorder iteration
+
+    def postorder(self):
+        """Generate a postorder iteration of positions in the tree."""
+        if not self.is_empty():
+            for p in self._subtree_postorder(self.root()):      # start recursion
+                yield p
+
+    def _subtree_postorder(self, p):
+        """Generate a postorder iteration of positions in subtree rooted at p."""
+        for c in self.children(p):                      # for each child c
+            for other in self._subtree_postorder(c):    # do postorder of c's subtree
+                yield other                             # yielding each to our caller
+        yield p                                         # visit p after its subtrees
+
 class BinaryTree(Tree):
     """Abstract base class representing a binary tree structure."""
 
