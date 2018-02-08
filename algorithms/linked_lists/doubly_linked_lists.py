8e9bbdd78b0a76b96aaec95f75a5b12810fa3838
Zhijun Sheng <zhijunsheng@gmail.com>
2018-02-07 22:57:22 -0500

    implemented FavoritesList

--- algorithms/linked_lists/doubly_linked_lists.py
+++ algorithms/linked_lists/doubly_linked_lists.py
@@ -211,6 +211,68 @@ def insertion_sort(L):
                 L.delete(pivot)
                 L.add_before(walk, value)   # reinsert value before walk
 
+class FavoritesList:
+    """List of elements ordered from most frequently accessed to least."""
+
+    class _Item:
+        __slots__ = '_value', '_count'      # streamline memeory usage
+        def __init__(self, e):
+            self._value = e                 # the user's element
+            self._count = 0                 # access count initially zero
+
+    def _find_position(self, e):
+        """Search for element e and return its Position (or None if not found)."""
+        walk = self._data.first()
+        while walk is not None and walk.element()._value != e:
+            walk = self._data.after(walk)
+        return walk
+
+    def _move_up(self, p):
+        """Move item at Position p earlier in the list based on access count."""
+        if p != self._data.first():                 # consider moving...
+            cnt = p.element()._count
+            walk = self._data.before(p)
+            if cnt > walk.element()._count:         # must shift forward
+                while (walk != self._data.first() and cnt > self._data.before(walk).element()._count):
+                    walk = self._data.before(walk)
+                self._data.add_before(walk, self._data.delete(p))   # delete/reinsert
+
+    def __init__(self):
+        """Create an empty list of favorites."""
+        self._data = PositionalList()                 # will be list of _Item instances
+
+    def __len__(self):
+        """Return number of entries on favorites list."""
+        return len(self._data)
+
+    def is_empty(self):
+        """Return True if list is empty."""
+        return len(self._data) == 0
+
+    def access(self, e):
+        """Access element e, thereby increasing its access count."""
+        p = self._find_position(e)              # try to locate existing element
+        if p is None:
+            p = self._data.add_last(self._Item(e))  # if new, place at end
+        p.element()._count += 1                     # always increment count
+        self._move_up(p)                            # consider moving forward
+
+    def remove(self, e):
+        """Remove element e from the list of favorites."""
+        p = self._find_position(e)                  # try to locate existing element
+        if p is not None:
+            self._data.delete(p)                    # delete, if found
+
+    def top(self, k):
+        """Generate sequence of top k elements in terms of access count."""
+        if not 1 <= k <= len(self):
+            raise ValueError('Illegal value for k')
+        walk = self._data.first()
+        for j in range(k):
+            item = walk.element()                   # element of list is _Item
+            yield item._value                       # report user's element
+            walk = self._data.after(walk)
+
 class DequeTests(unittest.TestCase):
 
     def test_deque(self):
@@ -241,6 +303,18 @@ class PositionalListTests(unittest.TestCase):
         pl.add_last(23)
         insertion_sort(pl)
         self.assertEqual(23, pl.after(pl.after(pl.first())).element())
+
+class FavoritesListTests(unittest.TestCase):
+
+    def test_favorites_list(self):
+        fl = FavoritesList()
+        fl.access(13)
+        fl.access(17)
+        fl.access(13)
+        fl.access(23)
+        fl.access(23)
+        fl.access(23)
+        self.assertEqual([23, 13, 17], list(fl.top(3)))
 
 if __name__ == '__main__':
     unittest.main()
